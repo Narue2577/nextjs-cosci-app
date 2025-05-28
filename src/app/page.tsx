91bfc313d0960eecfaf70c3ec3011d7cc3cfad9c
Narue2577 <naarue2@gmail.com>
2025-05-28 10:39:01 +0700

    shadcn added2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero02 from "@/components/hero-02/hero-02";
+import Features01Page from "@/components/features-01/features-01";
+import Footer04Page from "@/components/footer-04/footer-04";
 export default function Home() {
   return (
     <>
     <Hero02 />
-    
+    <Features01Page/>
+    <Footer04Page />
     </>
   );
 }
