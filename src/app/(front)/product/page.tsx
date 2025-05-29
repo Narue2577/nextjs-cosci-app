2836253eb86d27321706d7b32df8342c40d7f0e2
Narue2577 <naarue2@gmail.com>
2025-05-29 13:17:00 +0700

    install dotenv db

--- src/app/(front)/product/page.tsx
+++ src/app/(front)/product/page.tsx
@@ -8,11 +8,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import db from "@/db";
 import { ChevronRight } from "lucide-react";
+import { product } from "@/db/schema";
+import { desc } from "drizzle-orm";
 
-const Products = () => {
+const Products = async () => {
+  const products = await db.query.product.findMany({
+    orderBy: [desc(product.id)]
+  });
   return (
     <div className="max-w-screen-xl mx-auto py-16 px-6 xl:px-0">
+
+      <p>{JSON.stringify(products)}</p>
       <div className="flex items-end justify-between">
         <h2 className="text-3xl font-bold tracking-tight">Posts</h2>
         <Select defaultValue="recommended">
@@ -28,31 +36,24 @@ const Products = () => {
       </div>
 
       <div className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
-        {[0, 1, 2, 3, 4, 5, 6, 7].map((i) => (
-          <Card key={i} className="shadow-none overflow-hidden rounded-md">
+        {products.map((i) => (
+          <Card key={i.id} className="shadow-none overflow-hidden rounded-md">
             <CardHeader className="p-0">
               <div className="aspect-video bg-muted w-full border-b" />
             </CardHeader>
             <CardContent className="py-6">
               <div className="flex items-center gap-3">
                 <Badge className="bg-primary/5 text-primary hover:bg-primary/5 shadow-none">
-                  Technology
+                  ราคา {i.price} บาท
                 </Badge>
-                <span className="font-medium text-xs text-muted-foreground">
-                  5 min read
-                </span>
               </div>
 
               <h3 className="mt-4 text-[1.35rem] font-semibold tracking-tight">
-                A beginner&apos;s guide to blackchain for engineers
+                {i.title}
               </h3>
-              <p className="mt-2 text-muted-foreground">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse varius enim in eros.
-              </p>
 
               <Button className="mt-6 shadow-none">
-                Read more <ChevronRight />
+                หยิบใส่ตะกร้า  <ChevronRight />
               </Button>
             </CardContent>
           </Card>
